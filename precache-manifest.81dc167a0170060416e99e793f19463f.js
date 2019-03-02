00aff9581c443c9d304509824d575276722e1614
Anatoly Osipov <night.fire@mail.ru>
2019-03-02 23:22:27 +0500

    add

---
+++ precache-manifest.81dc167a0170060416e99e793f19463f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c43773cf4862b7c914f7",
+    "url": "/static/js/main.c43773cf.chunk.js"
+  },
+  {
+    "revision": "4f36e3b9b79050acf917",
+    "url": "/static/js/2.4f36e3b9.chunk.js"
+  },
+  {
+    "revision": "c43773cf4862b7c914f7",
+    "url": "/static/css/main.04394c0f.chunk.css"
+  },
+  {
+    "revision": "a3f83bbd162b829b66c37c2bc861bee9",
+    "url": "/index.html"
+  }
+];
