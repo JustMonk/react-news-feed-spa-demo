e096c639fcd00356d4ae930c2f674ec29b94d41a
Anatoly Osipov <night.fire@mail.ru>
2019-03-04 21:42:11 +0500

    fxd

--- precache-manifest.8c1a92f514f03393de8d09e14d5e3e09.js
+++ precache-manifest.8c1a92f514f03393de8d09e14d5e3e09.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "c43773cf4862b7c914f7",
-    "url": "/static/js/main.c43773cf.chunk.js"
+    "revision": "d3f742e595fc1d4b3f2e",
+    "url": "/static/js/main.d3f742e5.chunk.js"
   },
   {
     "revision": "4f36e3b9b79050acf917",
     "url": "/static/js/2.4f36e3b9.chunk.js"
   },
   {
-    "revision": "c43773cf4862b7c914f7",
-    "url": "/static/css/main.04394c0f.chunk.css"
+    "revision": "d3f742e595fc1d4b3f2e",
+    "url": "/static/css/main.393eafb9.chunk.css"
   },
   {
-    "revision": "a3f83bbd162b829b66c37c2bc861bee9",
+    "revision": "9bf3b38719e8332a414fd5d8dd93f17e",
     "url": "/index.html"
   }
 ];
